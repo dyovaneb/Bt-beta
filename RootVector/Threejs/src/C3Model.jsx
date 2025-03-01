f3e9a94bfd87535315ffae48f54063d15f524870
Hector <hector.math@gmail.com>
2025-03-01 17:48:25 -0300

    Opacidad

--- RootVector/Threejs/src/C3Model.jsx
+++ RootVector/Threejs/src/C3Model.jsx
@@ -7,7 +7,7 @@ import { MeshStandardMaterial } from "three"
 
 const redMaterial = new MeshStandardMaterial({ color: "red" });
 const greenMaterial = new MeshStandardMaterial({ color: "green" });
-const blueMaterial = new MeshStandardMaterial({ color: "blue", opacity: 0.5, transparent: true });
+const blueMaterial = new MeshStandardMaterial({ color: "blue", opacity: 0.2, transparent: true });
 const solidBlueMaterial = new MeshStandardMaterial({ color: "blue" });
 
 let setSelectedMeshState;
